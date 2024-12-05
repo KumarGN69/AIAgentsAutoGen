f7b508fe9a5741635e16f1344c7e9e82291982d8
kumarGN69 <kumargn1969@otmail.com>
2024-12-04 18:32:52 -0600

    Changes to App.py and too.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import autogen
 from autogen import AssistantAgent,UserProxyAgent, register_function
 from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
 from dotenv import load_dotenv
-from tool import get_news
+from tool import get_stocknews
 from pprint import pprint
 
 ##load the environment functions
@@ -13,8 +13,6 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 URL = os.getenv("BASE_URL")
 MODEL_NAME = os.getenv("MODEL_NAME")
-
-#llm_config = { "config_list": [{ "model": "gpt-4", "api_key": os.environ.get("OPENAI_API_KEY") }] }
 
 ##Define the model configuration details
 config_list= [
@@ -28,8 +26,6 @@ config_list= [
 
 ## Define the llm model with the requsite configuations
 llm_config = {
-            # "api_rate_limit":60.0,
-            # "seed":42,
             "config_list":config_list,
             "temperature":0
 }
@@ -39,36 +35,35 @@ assistant = AssistantAgent(
             name= "assistant", 
             llm_config=llm_config,
             
-            system_message="You stock trader"
+            system_message="You are a stock trader"
 
     )
 
-
+##Define User Proxy Agent
 user_proxy = UserProxyAgent(
     name="Stock_News_user_proxy",
     human_input_mode ="NEVER",
     max_consecutive_auto_reply=2,
     code_execution_config= False,
     llm_config=llm_config,
-    system_message="Reply TERMINATE if the response is complete else reply CONTINUE"
+    system_message="Reply TERMINATE if the response is complete else reply CONTINUE",
+    is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
     )
 
-task = """
-        get_news("TSLA",period="5d")
-       """
-# Start the chat
-# Register the calculator function to the two agents.
+# Register the function to be used by the two agents.
 register_function(
-    get_news,
+    get_stocknews,
     caller=assistant,  # The assistant agent can suggest calls to the calculator.
     executor=user_proxy,  # The user proxy agent can execute the calculator calls.
-    name="get_news",  # By default, the function name is used as the tool name.
+    name="get_stocknews",  # By default, the function name is used as the tool name.
     description="A news reporter",  # A description of the tool.
 )
 
+# Start the chat
 chat_results = user_proxy.initiate_chat(
     assistant,
-    message=" What is the latest news about TSLA in last 5d?"
+    message=" What are the important news items impacting the stock price of WMT in last 1m?"
 )
 
-pprint(chat_results.chat_history)
+#print the results
+# pprint(chat_results.chat_history)
--- tool.py
+++ tool.py
@@ -1,11 +1,6 @@
 import yfinance as yf
-from pprint import pprint
 
-def get_news(ticker_symbol:str,period:str) -> list[dict[str, any]] :
-    """Get mkt news of the ticker"""
-    pprint("Entered the get news function")
-    ticker =yf.Ticker(ticker_symbol)
+def get_stocknews(ticker_symbol: str, period: str) -> list[dict[str, any]]:
+    ticker = yf.Ticker(ticker_symbol)
     ticker.history(period=period)
     return ticker.get_news()
-
-# pprint(get_news("TSLA",period="5d"))
